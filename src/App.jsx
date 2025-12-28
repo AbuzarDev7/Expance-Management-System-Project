e9084aa15976ef400f5ab5b0c7563f1d922b08f2
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2025-12-28 19:45:11 +0500

    40% done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import ExpanceInp from './compunents/ExpanceInp'
+import React, { useState } from 'react'
+
+import HeaderBtns from './compunents/HeaderBtns'
 import ExpanceList from './compunents/ExpanceList'
+import ExpanceForm from './compunents/ExpanceForm'
 
 const App = () => {
+const [trasiction, setTransection] = useState([]);
+const cashIn = trasiction
+.filter((e)=> e.type === "in")
+.reduce((acc,cVal)=> acc + cVal.amount ,0)
+const cashOut = trasiction
+.filter((e)=> e.type === "out")
+.reduce((acc,cVal)=> acc + cVal.amount ,0)
+const balance = cashIn - cashOut
   return (
  <>
-    
-      <ExpanceInp/>
+  <h1 className="text-5xl font-extrabold mb-10 text-center  drop-shadow-lg">
+  Expense Management System
+</h1>
+
+
+    <HeaderBtns  cashIn={cashIn} cashOut={cashOut} balance={balance}/>
+      <ExpanceForm 
+          transaction={trasiction} 
+          setTransaction={setTransection}
+        />
+        <br />
+    <ExpanceList/>
     
  </>
  
